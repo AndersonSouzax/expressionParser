18c0c770f91c5196fe5e40abc6714109e7a0737e
Anderson <anderson.souzax@hotmail.com>
2017-11-13 10:16:17 -0300

    adicionando css

--- src/javascript/parser.js
+++ src/javascript/parser.js
@@ -2,36 +2,6 @@ class Parser{
 
     static testExpression(exp){
 
-        if(exp.includes('(')){
-			if(exp.includes(')')){
-				//Checking if '(' appears same number of times as ')'
-				if(cont.match(/\(/g).length != cont.match(/\)/g).length){
-					return false;
-				}else{
-					let open = [];
-                    let iter = 0;
-					while(iter != exp.length){
-						if(exp[iter] == '('){
-							open.push('(');
-							iter++;
-						}else if(exp[iter] == ')'){
-							if(open.length < 1){
-								return false;
-							}else{
-								open.pop();
-								iter++;
-							}
-						}else{
-							iter++;
-							continue;
-						}
-					}
-				}
-			}else{
-				return false;
-			}
-		}
-
 		let it = 0, expLength = exp.length, finalValue = null;
 
 		let acepTypes = ['identifier','functionIdentifier',
--- src/javascript/terminal.js
+++ src/javascript/terminal.js
@@ -1,5 +1,30 @@
 import Parser from './parser.js';
 
+let globalExpression = "", oldGlobalExpression = "";
+
+let intervalFunction = () => {
+
+    if(globalExpression != oldGlobalExpression){
+
+        let body = document.body;
+
+        if( parenTest(globalExpression) ){
+
+            let res = Parser.testExpression(globalExpression);
+
+        }else{
+            if(body.classList.contains('valid-exp')){
+                body.classList.remove('valid-exp');
+            }
+            body.classList.add('invalid-exp');
+        }
+    }
+};
+
+//Digest cycle, like angular
+setInterval(intervalFunction, 500);
+
+
 let isNumber = (number) => {
     return /[0-9]+/.test(number);
 };
@@ -11,4 +36,39 @@ let isPar = (text) => {
 let isOperator = (ope) => {
     let operators = ['*','/','+','-'];
     return operators.includes(ope);
+};
+
+let parenTest = (exp) => {
+
+    if(exp.includes('(')){
+        if(exp.includes(')')){
+            //Checking if '(' appears same number of times as ')'
+            if(cont.match(/\(/g).length != cont.match(/\)/g).length){
+                return false;
+            }else{
+
+                let open = [];
+                let iter = 0;
+                while(iter != exp.length){
+                    if(exp[iter] == '('){
+                        open.push('(');
+                        iter++;
+                    }else if(exp[iter] == ')'){
+                        if(open.length < 1){
+                            return false;
+                        }else{
+                            open.pop();
+                            iter++;
+                        }
+                    }else{
+                        iter++;
+                        continue;
+                    }
+                }
+            }
+        }else{
+            return false;
+        }
+    }
+    return true;
 };
